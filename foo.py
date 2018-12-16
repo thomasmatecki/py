5924ed02f0e9ecfdfc74e04130dff1760654237d
Thomas Matecki <thomas.matecki@gmail.com>
2018-12-16 13:13:30 -0500

    save

---
+++ foo.py
@@ -0,0 +1,9 @@
+import bar
+from types import CodeType
+
+CodeType(
+
+)
+
+
+print("hello world %d" % bar.add(1, 2))
---
+++ pyj_compire.py
@@ -0,0 +1,21 @@
+from types import CodeType
+
+code = CodeType(
+    0,
+    0,
+    0,
+    int(),
+    int(),
+    bytes(),
+    (),
+    (),
+    (),
+    str(),
+    str(),
+    int(),
+    bytes(),
+    (),
+    ()
+)
+
+pass
